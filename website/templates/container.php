9491dbfd3a76e765a9f76ba7ec90fab64192a60c
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2022-12-29 16:27:04 +0100

    Add a favicon to the website

--- website/templates/container.php
+++ website/templates/container.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>AllGames <?php echo (isset($templateParams["page-title"]) ? "- ".$templateParams["page-title"] : "" ); ?> </title>
-
+    <link rel="icon" type="image/x-icon" href="inc/img/liked.png">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
     <link rel="stylesheet" href="inc/css/style.css" />
 </head>
