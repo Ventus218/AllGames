dfeabb490172d477c985b2057985019c0348224a
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2023-01-04 11:13:39 +0100

    Require_once of session.php

--- website/api/toggle-mi-piace.php
+++ website/api/toggle-mi-piace.php
@@ -1,12 +1,13 @@
 <?php
 
 require_once(__DIR__."/../bootstrap.php");
+require_once(__DIR__."/../inc/php/session.php");
 
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
     if (!checkIfSessionIsActive()) {
         redirectToLogin($_SERVER["REQUEST_URI"]);
     } else {
-        echo "bravo";
+        
     }
 } else {
     internalServerError("Sono accettate solo richieste POST");
