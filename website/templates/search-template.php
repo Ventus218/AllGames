7ec8d1265d80a7bcbc060c27d8d443bf2635cff3
Corrado Stortini <corrado.stortini2@studio.unibo.it>
2023-01-10 12:22:19 +0100

    Fix input of search form

--- website/templates/search-template.php
+++ website/templates/search-template.php
@@ -2,12 +2,13 @@
     <form action="#" method="post" class="px-2 mt-2">
     <div class="row me-1">
         <div class="col-11 pe-0">
-            <label for="search" class="visually-hidden">Cerca un username o una community</label>
-            <input class="form-control mb-3" type="text" name="search" id="search" placeholder="Cerca un username o una community" required />
+            <label for="search" class="visually-hidden">Cerca degli utenti, delle community o dei tag</label>
+            <input class="form-control mb-3" type="text" name="search" id="search" placeholder="Cerca degli utenti, delle community o dei tag" required />
         </div>
         <div class="col-1 ps-0">
-            <label class="visually-hidden" for="search-button">Tasto per cercare</label>
-            <input type="image" class="opacity-50 search-img" alt="Tasto per cercare" src="inc/img/search.png" id="search-button"/>
+            <button type="submit" class="btn p-0">
+                <img class="search-img opacity-50" alt="Tasto per cercare" src="inc/img/search.png" />
+            </button>
         </div>
     </div>
     </form>
